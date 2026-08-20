6e56682a4460f3f12101c6d9ce61d23a82f25f30
Phillip Chin <ekkus93@gmail.com>
2026-08-19 22:28:00 -0700

    asr: expose Moonshine model installer

--- src-tauri/src/asr/moonshine/mod.rs
+++ src-tauri/src/asr/moonshine/mod.rs
@@ -1,8 +1,15 @@
 mod ffi;
+mod installer;
 mod manifest;
 mod runtime;
 
 pub use runtime::{
     MoonshineLine, MoonshineModelArchitecture, MoonshineStream, MoonshineTranscriber,
     MoonshineTranscript,
+};
+
+pub use installer::{
+    MoonshineModelInstallCancellation, MoonshineModelInstallDisposition,
+    MoonshineModelInstallError, MoonshineModelInstallErrorKind, MoonshineModelInstallOutcome,
+    MoonshineModelInstaller,
 };
